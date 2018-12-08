51b2f77b4e342b7eba5f38867934f0f501bbaba5
gabchak <andrey.gabchak@gmail.com>
2018-12-08 12:05:07 +0200

    Added annotation hibernate annotation and relation

--- src/main/java/com/mate/hackathon/aug2018/ecommerce/model/Product.java
+++ src/main/java/com/mate/hackathon/aug2018/ecommerce/model/Product.java
@@ -1,13 +1,26 @@
 package com.mate.hackathon.aug2018.ecommerce.model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.mate.hackathon.aug2018.ecommerce.controller.model.dto.ProductDto;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "PRODUCTS")
 public class Product {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "NAME")
     private String name;
+    @Column(name = "DESCRIPTION")
     private String description;
+    @Column(name = "PRICE")
     private double price;
+    @JoinColumn(name = "FK_CATEGORIES")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonBackReference
     private Category category;
 
     public static Product of(ProductDto productDto) {
