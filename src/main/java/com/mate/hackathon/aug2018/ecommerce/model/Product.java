099c8cb335420c79471448ca11fafbcc9c44573b
gabchak <andrey.gabchak@gmail.com>
2018-12-08 11:12:53 +0200

    Created Product entity

---
+++ src/main/java/com/mate/hackathon/aug2018/ecommerce/model/Product.java
@@ -0,0 +1,50 @@
+package com.mate.hackathon.aug2018.ecommerce.model;
+
+public class Product {
+
+    private Long id;
+    private String name;
+    private String description;
+    private double price;
+    private Category category;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+}
