f8cb4c2617fa2f2e5a74959a2022282579a07cb9
gabchak <andrey.gabchak@gmail.com>
2018-12-08 18:07:26 +0200

    Added methods: findByUser, save, increaseProductQuantity, decreaseProductQuantity, deleteProduct

--- eCommerce-Server/src/main/java/com/mate/hackathon/aug2018/ecommerce/service/CartService.java
+++ eCommerce-Server/src/main/java/com/mate/hackathon/aug2018/ecommerce/service/CartService.java
@@ -1,5 +1,20 @@
 package com.mate.hackathon.aug2018.ecommerce.service;
 
+import com.mate.hackathon.aug2018.ecommerce.model.Cart;
+import com.mate.hackathon.aug2018.ecommerce.model.Product;
+import com.mate.hackathon.aug2018.ecommerce.model.User;
+
+import java.util.Optional;
+
 public interface CartService {
 
+    Optional<Cart> findByUser(User user);
+
+    Cart save(Cart cart);
+
+    void increaseProductQuantity(Product product, User user);
+
+    void decreaseProductQuantity(Product product, User user);
+
+    void deleteProduct(Product product, User user);
 }
--- eCommerce-Server/src/main/java/com/mate/hackathon/aug2018/ecommerce/service/CartServiceImpl.java
+++ eCommerce-Server/src/main/java/com/mate/hackathon/aug2018/ecommerce/service/CartServiceImpl.java
@@ -1,4 +1,62 @@
 package com.mate.hackathon.aug2018.ecommerce.service;
 
+import com.mate.hackathon.aug2018.ecommerce.model.Cart;
+import com.mate.hackathon.aug2018.ecommerce.model.Product;
+import com.mate.hackathon.aug2018.ecommerce.model.User;
+import com.mate.hackathon.aug2018.ecommerce.repository.CartRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
 public class CartServiceImpl implements CartService {
+
+    @Autowired
+    private CartRepository cartRepository;
+
+    @Override
+    public Optional<Cart> findByUser(User user) {
+        return cartRepository.findByUser_Id(user.getId());
+    }
+
+    @Override
+    public Cart save(Cart cart) {
+        return cartRepository.save(cart);
+    }
+
+    @Override
+    public void increaseProductQuantity(Product product, User user) {
+        Optional<Cart> cartFromDB = cartRepository.findByUser_Id(user.getId());
+        Cart cart;
+        Integer defaultProductQuantity = 1;
+        if (cartFromDB.isPresent()) {
+            cart = cartFromDB.get();
+            cart.increaseQuantity(product, defaultProductQuantity);
+        } else {
+            cart = new Cart();
+            cart.setUser(user);
+            cart.increaseQuantity(product, defaultProductQuantity);
+        }
+        cartRepository.save(cart);
+    }
+
+    @Override
+    public void decreaseProductQuantity(Product product, User user) {
+        Optional<Cart> cartFromDB = cartRepository.findByUser_Id(user.getId());
+        if (cartFromDB.isPresent()) {
+            Cart cart = cartFromDB.get();
+            Integer defaultProductQuantity = 1;
+            cart.decreaseQuantity(product, defaultProductQuantity);
+            cartRepository.save(cart);
+        }
+    }
+
+    @Override
+    public void deleteProduct(Product product, User user) {
+        Optional<Cart> optionalCart = cartRepository.findByUser_Id(user.getId());
+        if (optionalCart.isPresent()) {
+            Cart cartFromDB = optionalCart.get();
+            cartFromDB.deleteProduct(product);
+            cartRepository.save(cartFromDB);
+        }
+    }
 }
