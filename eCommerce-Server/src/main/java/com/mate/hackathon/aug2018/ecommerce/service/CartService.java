84325ea05ea9517488d8ef3fd0588f85566826fb
gabchak <andrey.gabchak@gmail.com>
2018-12-08 15:53:47 +0200

    Create services

---
+++ eCommerce-Server/src/main/java/com/mate/hackathon/aug2018/ecommerce/service/CartService.java
@@ -0,0 +1,5 @@
+package com.mate.hackathon.aug2018.ecommerce.service;
+
+public interface CartService {
+
+}
---
+++ eCommerce-Server/src/main/java/com/mate/hackathon/aug2018/ecommerce/service/CartServiceImpl.java
@@ -0,0 +1,4 @@
+package com.mate.hackathon.aug2018.ecommerce.service;
+
+public class CartServiceImpl implements CartService {
+}
