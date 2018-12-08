62d146a82a1217dd92365eb2e7cefe259de05c7d
Dkhmil <0973138620dh@gmail.com>
2018-12-08 17:30:49 +0200

    changing UserController

--- eCommerce-Server/src/main/java/com/mate/hackathon/aug2018/ecommerce/controller/UserController.java
+++ eCommerce-Server/src/main/java/com/mate/hackathon/aug2018/ecommerce/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.mate.hackathon.aug2018.ecommerce.controller;
 
+import com.mate.hackathon.aug2018.ecommerce.model.User;
 import com.mate.hackathon.aug2018.ecommerce.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -20,25 +21,29 @@ class UserController {
     private UserService service;
 
     @GetMapping("/users")
-    List<com.mate.hackathon.aug2018.ecommerce.model.User> getAll() {
+    List<User> getAll() {
         return service.getAll();
     }
 
     @PostMapping("/users")
-    com.mate.hackathon.aug2018.ecommerce.model.User addUser(@RequestBody com.mate.hackathon.aug2018.ecommerce.model.User user) {
-        return null;
+    User addUser(@RequestBody User user) {
+        return service.create(user);
     }
 
     @GetMapping("/users/{id}")
-    Optional<com.mate.hackathon.aug2018.ecommerce.model.User> getUser(@PathVariable Long id) {
-        return null;
+    Optional<User> getUser(@PathVariable Long id) {
+        return service.getById(id);
     }
 
     @PutMapping("/user/{id}")
+    public User updateUser(@RequestBody User user, @PathVariable Long id) {
+        return service.update(user, id);
+    }
 
 
     @DeleteMapping("/user/{id}")
-    void deleteEmployee(@PathVariable Long id) {
+    void delete(@PathVariable Long id) {
+        service.delete(id);
     }
 
 }
