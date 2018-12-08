c5a6f7aeab9b582bddce3969fa1d4c750e18b6d2
gabchak <andrey.gabchak@gmail.com>
2018-12-08 16:51:25 +0200

    Added method find cart by users id

--- eCommerce-Server/src/main/java/com/mate/hackathon/aug2018/ecommerce/repository/CartRepository.java
+++ eCommerce-Server/src/main/java/com/mate/hackathon/aug2018/ecommerce/repository/CartRepository.java
@@ -3,6 +3,10 @@ package com.mate.hackathon.aug2018.ecommerce.repository;
 import com.mate.hackathon.aug2018.ecommerce.model.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CartRepository extends JpaRepository<Cart, Long> {
+
+    Optional<Cart> findByUser_Id(Long userId); //User in Cart.class but search by User.Id
 
 }
