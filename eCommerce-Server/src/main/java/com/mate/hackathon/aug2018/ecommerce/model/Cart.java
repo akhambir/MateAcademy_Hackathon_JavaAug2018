95d4ec9686477cd6b0b46280877f60f0c3528346
gabchak <andrey.gabchak@gmail.com>
2018-12-08 15:44:04 +0200

    Created Cart entity

---
+++ eCommerce-Server/src/main/java/com/mate/hackathon/aug2018/ecommerce/model/Cart.java
@@ -0,0 +1,35 @@
+package com.mate.hackathon.aug2018.ecommerce.model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Cart {
+
+    private User customer;
+    private Map<Product, Integer> products = new HashMap<>();
+    private Double amount;
+
+    public User getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(User customer) {
+        this.customer = customer;
+    }
+
+    public Map<Product, Integer> getProducts() {
+        return products;
+    }
+
+    public void setProducts(Map<Product, Integer> products) {
+        this.products = products;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+}
