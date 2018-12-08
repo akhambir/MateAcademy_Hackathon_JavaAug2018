9bf36dd521024bd96e4a70ec88195e78f14b6150
Gleb394 <Glebas3@gmail.com>
2018-12-08 18:53:05 +0200

    Controller method update

--- eCommerce-Server/src/main/java/com/mate/hackathon/aug2018/ecommerce/controller/CategoryController.java
+++ eCommerce-Server/src/main/java/com/mate/hackathon/aug2018/ecommerce/controller/CategoryController.java
@@ -5,9 +5,7 @@ import com.mate.hackathon.aug2018.ecommerce.model.Category;
 import com.mate.hackathon.aug2018.ecommerce.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -26,7 +24,7 @@ public class CategoryController {
                 .orElseGet(ResponseEntity.notFound()::build);
     }
 
-    @GetMapping(name = "/{categoryName}")
+    @GetMapping(name = "/{Name}")
     public ResponseEntity<CategoryDto> getCategory(@PathVariable String name) {
         return categoryService.findByName(name)
                 .map(CategoryDto::of)
@@ -34,13 +32,26 @@ public class CategoryController {
                 .orElseGet(ResponseEntity.notFound()::build);
     }
 
-    @GetMapping(name = "/admin/category")
-    public ResponseEntity<List<CategoryDto>> save(CategoryDto categoryDto) {
+    @PostMapping(name = "/admin/category/")
+    public ResponseEntity<List<CategoryDto>> saveCategory(CategoryDto categoryDto) {
         categoryService.save(Category.of(categoryDto));
         return getAllCategory();
     }
 
-    @GetMapping(name = "/admin/category")
+    @PutMapping(name = "/admin/category/{name}")
+    public ResponseEntity<List<CategoryDto>> updateCategory(@RequestBody CategoryDto category, @PathVariable String name) {
+        Category categoryFromFront = Category.of(category);
+        Optional<Category> optionalCategory = categoryService.findByName(name);
+        Category oldCategory = null;
+        if (optionalCategory.isPresent()) {
+            oldCategory = optionalCategory.get();
+        }
+        oldCategory.setName(categoryFromFront.getName());
+        categoryService.save(oldCategory);
+        return getAllCategory();
+    }
+
+    @DeleteMapping(name = "/admin/category/{name}")
     public ResponseEntity<List<CategoryDto>> deleteByName(@PathVariable String name) {
         categoryService.deleteByName(name);
         return getAllCategory();
