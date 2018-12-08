6497c0231baa262e503bbd463f6564ae14840454
Dkhmil <0973138620dh@gmail.com>
2018-12-08 18:27:42 +0200

    changed updateMethod

--- eCommerce-Server/src/main/java/com/mate/hackathon/aug2018/ecommerce/service/UserService.java
+++ eCommerce-Server/src/main/java/com/mate/hackathon/aug2018/ecommerce/service/UserService.java
@@ -1,16 +1,15 @@
 package com.mate.hackathon.aug2018.ecommerce.service;
 
 import com.mate.hackathon.aug2018.ecommerce.model.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface UserService {
-    Optional<User> getByEmail(String email);
-
+public interface UserService extends UserDetailsService {
     User create(User user);
 
-    User update(User user,Long id);
+    User update(User user);
 
     void delete(Long id);
 
