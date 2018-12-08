21bc93e8551c744b7581571fb9a6a668692a06f6
gabchak <andrey.gabchak@gmail.com>
2018-12-08 18:36:24 +0200

    Methods renaming

--- eCommerce-Server/src/main/java/com/mate/hackathon/aug2018/ecommerce/controller/CartController.java
+++ eCommerce-Server/src/main/java/com/mate/hackathon/aug2018/ecommerce/controller/CartController.java
@@ -8,6 +8,7 @@ import com.mate.hackathon.aug2018.ecommerce.model.User;
 import com.mate.hackathon.aug2018.ecommerce.service.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,8 +20,14 @@ public class CartController {
     private CartService cartService;
 
     @GetMapping("/buy")
-    public ResponseEntity<CartDto> addProduct(@RequestBody ProductDto productDto, User user) {
+    public ResponseEntity<CartDto> addProductToCart(@RequestBody ProductDto productDto, User user) {
         Cart cart = cartService.increaseProductQuantity(Product.of(productDto), user);
+        return ResponseEntity.ok(CartDto.of(cart));
+    }
+
+    @DeleteMapping("/cart/delete")
+    public ResponseEntity<CartDto> deleteProductToCart(@RequestBody ProductDto productDto, User user) {
+        Cart cart = cartService.deleteProduct(Product.of(productDto), user);
         return ResponseEntity.ok(CartDto.of(cart));
     }
 }
