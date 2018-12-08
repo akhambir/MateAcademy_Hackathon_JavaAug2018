986c682f2847f49c29a956635fbc6c3d98842450
gabchak <andrey.gabchak@gmail.com>
2018-12-08 18:30:49 +0200

    Added method 'addProduct'

--- eCommerce-Server/src/main/java/com/mate/hackathon/aug2018/ecommerce/controller/CartController.java
+++ eCommerce-Server/src/main/java/com/mate/hackathon/aug2018/ecommerce/controller/CartController.java
@@ -1,14 +1,26 @@
 package com.mate.hackathon.aug2018.ecommerce.controller;
 
-import com.mate.hackathon.aug2018.ecommerce.repository.CartRepository;
+import com.mate.hackathon.aug2018.ecommerce.controller.model.dto.CartDto;
+import com.mate.hackathon.aug2018.ecommerce.controller.model.dto.ProductDto;
+import com.mate.hackathon.aug2018.ecommerce.model.Cart;
+import com.mate.hackathon.aug2018.ecommerce.model.Product;
+import com.mate.hackathon.aug2018.ecommerce.model.User;
+import com.mate.hackathon.aug2018.ecommerce.service.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CartController {
 
     @Autowired
-    private CartRepository cartRepository;
+    private CartService cartService;
 
-
+    @GetMapping("/buy")
+    public ResponseEntity<CartDto> addProduct(@RequestBody ProductDto productDto, User user) {
+        Cart cart = cartService.increaseProductQuantity(Product.of(productDto), user);
+        return ResponseEntity.ok(CartDto.of(cart));
+    }
 }
