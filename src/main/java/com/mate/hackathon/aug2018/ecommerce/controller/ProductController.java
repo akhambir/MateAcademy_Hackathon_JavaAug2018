89faadba50235c6f94a6ab4da2a84c53617b40bb
gabchak <andrey.gabchak@gmail.com>
2018-12-08 12:44:51 +0200

    Created product controller and method 'findAll'

---
+++ src/main/java/com/mate/hackathon/aug2018/ecommerce/controller/ProductController.java
@@ -0,0 +1,28 @@
+package com.mate.hackathon.aug2018.ecommerce.controller;
+
+import com.mate.hackathon.aug2018.ecommerce.controller.model.dto.ProductDto;
+import com.mate.hackathon.aug2018.ecommerce.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/admin/products")
+    public ResponseEntity<List<ProductDto>> getAllProducts() {
+        return Optional.of(productService.findAll())
+                .map(ProductDto::of)
+                .map(ResponseEntity::ok)
+                .orElseGet(ResponseEntity.notFound()::build);
+    }
+
+}
