bfdca800201a87dd9953a2c16cf02802a314b784
gabchak <andrey.gabchak@gmail.com>
2018-12-08 12:44:15 +0200

    Removed unnecessary modificator 'public'

--- src/main/java/com/mate/hackathon/aug2018/ecommerce/repository/ProductDao.java
+++ src/main/java/com/mate/hackathon/aug2018/ecommerce/repository/ProductDao.java
@@ -7,5 +7,5 @@ import java.util.Optional;
 
 public interface ProductDao extends JpaRepository<Product, Long> {
 
-    public Optional<Product> findByProductCode(Long productCode);
+    Optional<Product> findByProductCode(Long productCode);
 }
