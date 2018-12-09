dfe3e69b7e774c131b69d96825e6e98233b2e0dc
gabchak <andrey.gabchak@gmail.com>
2018-12-09 18:14:02 +0200

    Init commit

--- eCommerce-Server/src/main/java/com/mate/hackathon/aug2018/ecommerce/model/Order.java
+++ eCommerce-Server/src/main/java/com/mate/hackathon/aug2018/ecommerce/model/Order.java
@@ -1,4 +1,72 @@
 package com.mate.hackathon.aug2018.ecommerce.model;
 
+import java.time.LocalDate;
+import java.util.Map;
+
 public class Order {
+
+    private Long id;
+    private String orderCode;
+    private User user;
+    private LocalDate orderDate;
+    private Double orderAmount;
+    private String orderComment;
+    private Map<Product, Integer> products; //relation product - quantity
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getOrderCode() {
+        return orderCode;
+    }
+
+    public void setOrderCode(String orderCode) {
+        this.orderCode = orderCode;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public LocalDate getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDate orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public Double getOrderAmount() {
+        return orderAmount;
+    }
+
+    public void setOrderAmount(Double orderAmount) {
+        this.orderAmount = orderAmount;
+    }
+
+    public String getOrderComment() {
+        return orderComment;
+    }
+
+    public void setOrderComment(String orderComment) {
+        this.orderComment = orderComment;
+    }
+
+    public Map<Product, Integer> getProducts() {
+        return products;
+    }
+
+    public void setProducts(Map<Product, Integer> products) {
+        this.products = products;
+    }
 }
