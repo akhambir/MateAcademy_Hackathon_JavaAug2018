2f80a192d0e24789aae397eadef0763eb8a81830
gabchak <andrey.gabchak@gmail.com>
2018-12-09 17:42:55 +0200

    Used Principle to get user data

--- eCommerce-Server/src/main/java/com/mate/hackathon/aug2018/ecommerce/controller/CartController.java
+++ eCommerce-Server/src/main/java/com/mate/hackathon/aug2018/ecommerce/controller/CartController.java
@@ -6,23 +6,28 @@ import com.mate.hackathon.aug2018.ecommerce.model.Cart;
 import com.mate.hackathon.aug2018.ecommerce.model.Product;
 import com.mate.hackathon.aug2018.ecommerce.model.User;
 import com.mate.hackathon.aug2018.ecommerce.service.CartService;
+import com.mate.hackathon.aug2018.ecommerce.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.util.Optional;
 
 @RestController
 public class CartController {
 
     @Autowired
     private CartService cartService;
-
+    @Autowired
+    private UserService userService;
     @PostMapping("/buy")
-    public ResponseEntity<CartDto> addProductToCart(@RequestBody ProductDto productDto, Principal principal, Integer quantity) {
-        return null;
-//        Cart cart = cartService.setProductQuantity(Product.of(productDto), user, quantity);
-//        return ResponseEntity.ok(CartDto.of(cart));
+    public ResponseEntity<CartDto> addProductToCart(@RequestBody ProductDto productDto, Principal principal) {
+        String name = principal.getName();
+        Optional<User> byEmail = userService.getByEmail(name);
+        User user = byEmail.get();
+        Cart cart = cartService.setProductQuantity(Product.of(productDto), productDto.getQuantity(), productDto.getCategoryName(), user);
+        return ResponseEntity.ok(CartDto.of(cart));
     }
 
     @DeleteMapping("/cart/deleteProduct")
