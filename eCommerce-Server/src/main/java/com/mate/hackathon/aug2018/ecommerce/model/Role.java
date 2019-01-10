2cfedea2d8b21eb2db9efe7cf118e5701eb4b1bc
gabchak <andrey.gabchak@gmail.com>
2019-01-10 22:32:41 +0200

    Replaced many classes of javax.persistence to *

--- eCommerce-Server/src/main/java/com/mate/hackathon/aug2018/ecommerce/model/Role.java
+++ eCommerce-Server/src/main/java/com/mate/hackathon/aug2018/ecommerce/model/Role.java
@@ -1,15 +1,6 @@
 package com.mate.hackathon.aug2018.ecommerce.model;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
